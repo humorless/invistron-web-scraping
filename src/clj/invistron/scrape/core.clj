7a636498fc7f9c9d358af76695e0de7d12ffde72
Laurence Chen <humorless@gmail.com>
2019-10-02 16:23:54 +0800

    integrate level 1 crawling and level 2 scrape

--- src/clj/invistron/scrape/core.clj
+++ src/clj/invistron/scrape/core.clj
@@ -24,16 +24,25 @@
           "div.product-list div.product-name a[href]"  ;; Jsoup selector
           (soup/attr "abs:href"))
 
-(defn get-product-urls [url]
-  (soup/$ (soup/get! url)
-          "div.product-list div.product-name a[href]"  ;; Jsoup selector
-          (soup/attr "abs:href")))                     ;; attribute selector
-
-(defn all-product-urls []
-  (map get-product-urls (all-page-urls)))
+#_(defn get-product-urls [url]
+    (soup/$ (soup/get! url)
+            "div.product-list div.product-name a[href]"  ;; Jsoup selector
+            (soup/attr "abs:href")))                     ;; attribute selector
 
 (def driver (w/chrome-headless))
-(w/go driver "https://invistron.en.taiwantrade.com/product-catalog/all-products.html?page=1&rows=20&productType=&sidx=&sord=&listDisplayMode=detail")
-(w/wait-visible driver {:fn/has-classes [:template :product-list]})
-(map #(w/get-element-attr-el driver % "href")
-     (w/query-all driver {:css "div.product-list div.product-name a"}))
+
+(defn scrape-product-urls-by-browser [url]
+  (log/info "begin to scrape " url)
+  (w/go driver url)
+  (w/wait-visible driver {:fn/has-classes [:template :product-list]})
+  (map #(w/get-element-attr-el driver % "href")
+       (w/query-all driver {:css "div.product-list div.product-name a"})))
+
+(defn scrape-and-spit [url]
+  (prn "running ...")
+  (doseq [product-url (scrape-product-urls-by-browser url)]
+    (spit "url.edn" (prn-str product-url) :append true)))
+
+(defn process []
+  (let [p-urls (all-page-urls)]
+    (map scrape-and-spit p-urls)))
