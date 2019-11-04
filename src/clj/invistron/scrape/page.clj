671be95152c32d30d7912250f5d70566906f18d6
Laurence Chen <humorless@gmail.com>
2019-11-05 05:10:27 +0800

    add download-image to page.clj

--- src/clj/invistron/scrape/page.clj
+++ src/clj/invistron/scrape/page.clj
@@ -5,7 +5,8 @@
    [jsoup.soup :as soup]
    [invistron.config :refer [env]]
    [clojure.tools.logging :as log]
-   [yaml.core :as yaml]))
+   [yaml.core :as yaml]
+   [clj-http.client :as client]))
 
 (defn read-urls []
   (with-open [rdr (io/reader "resources/url.edn")]
@@ -40,6 +41,13 @@
    (string/replace url #"https://invistron.en.taiwantrade.com/product/" "")
    #"html"
    "md"))
+
+(defn download-image [url]
+  (let [img-url (get-img-url url)
+        filename (string/replace img-url #"http.*/" "")]
+    (clojure.java.io/copy
+     (:body (client/get img-url {:as :stream}))
+     (java.io.File. (str "image/" filename)))))
 
 (defn gen-md-file [url]
   (let [f (->filename url)
